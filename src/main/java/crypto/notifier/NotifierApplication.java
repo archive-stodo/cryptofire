2849814dd589ed61f5266c1aa816fea982474347
Dawid Stodołkiewicz <dawid.stodolkiewicz@empirica.pl>
2018-07-28 22:42:53 +0200

    flyway

--- src/main/java/crypto/notifier/NotifierApplication.java
+++ src/main/java/crypto/notifier/NotifierApplication.java
@@ -1,12 +1,20 @@
 package crypto.notifier;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class NotifierApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(NotifierApplication.class, args);
+//		Flyway flyway = new Flyway();
+//		flyway.setDataSource("jdbc:h2:mem:testdb", "sa", null);
+//		flyway.migrate();
 	}
 }
