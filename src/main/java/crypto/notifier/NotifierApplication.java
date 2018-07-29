88c92a7b9837f7380ab84180b119347fd58c5e4e
Dawid Stodołkiewicz <dawid.stodolkiewicz@empirica.pl>
2018-07-29 22:25:49 +0200

    removed unused properties, flyway setup

--- src/main/java/crypto/notifier/NotifierApplication.java
+++ src/main/java/crypto/notifier/NotifierApplication.java
@@ -1,6 +1,5 @@
 package crypto.notifier;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,12 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class NotifierApplication {
 
 	public static void main(String[] args) {
-
-		Flyway flyway = new Flyway();
-		flyway.setDataSource("jdbc:postgresql://localhost:5432/notifierdb", "postgres", "admin123");
-		flyway.setInitOnMigrate(true);
-		flyway.migrate();
-
 		SpringApplication.run(NotifierApplication.class, args);
 	}
 }
