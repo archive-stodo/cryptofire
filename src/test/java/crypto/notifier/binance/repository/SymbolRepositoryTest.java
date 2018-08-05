37a213727e0985739d3fe8cffe0c0eee135a1e3e
Dawid Stodołkiewicz <dawid.stodolkiewicz@empirica.pl>
2018-08-05 19:05:14 +0200

    add devtools, refac

--- src/test/java/crypto/notifier/binance/repository/SymbolRepositoryTest.java
+++ src/test/java/crypto/notifier/binance/repository/SymbolRepositoryTest.java
@@ -1,6 +1,8 @@
 package crypto.notifier.binance.repository;
 
 import crypto.notifier.binance.entity.Symbol;
+import org.h2.tools.Server;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -27,15 +30,15 @@ public class SymbolRepositoryTest {
     public void findAllTest(){
         //given
         Symbol symbol = new Symbol();
-        symbol.setId(1);
         symbol.setSymbolName("symbolName");
         symbol.setCurrentAskPrice(BigDecimal.ONE);
         symbol.setCurrentBidPrice(BigDecimal.TEN);
-        symbolRepository.save(symbol);
-        symbolRepository.flush();
+        entityManager.persistAndFlush(symbol);
 
+        //when
         List<Symbol> symbolsFound = symbolRepository.findAll();
 
+        //then
         assertEquals(1, symbolsFound.size());
         assertEquals(symbolsFound.get(0).getCurrentBidPrice(), BigDecimal.TEN);
     }
